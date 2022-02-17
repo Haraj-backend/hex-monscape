841e9e1ffb90150a9f332f315c036af55527b619
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-02-17 19:52:36 +0300

    [build] finished game struct implementation;

--- internal/domain/playing/game.go
+++ internal/domain/playing/game.go
@@ -1,6 +1,10 @@
 package playing
 
-import "github.com/riandyrn/pokebattle/internal/domain/entity"
+import (
+	"github.com/google/uuid"
+	"github.com/riandyrn/pokebattle/internal/domain/entity"
+	"gopkg.in/validator.v2"
+)
 
 type Scenario string
 
@@ -12,20 +16,60 @@ const (
 )
 
 type Game struct {
-	ID         string
-	PlayerName string
-	Partner    entity.Pokemon
-	CreatedAt  int64
-	BattleWon  int
-	Scenario   Scenario
+	ID         string         `json:"id"`
+	PlayerName string         `json:"player_name"`
+	Partner    entity.Pokemon `json:"partner"`
+	CreatedAt  int64          `json:"created_at"`
+	BattleWon  int            `json:"battle_won"`
+	Scenario   Scenario       `json:"scenario"`
 }
 
-func (g *Game) GetNextScenario() Scenario {
-	// TODO
-	return ""
+type GameConfig struct {
+	PlayerName string         `validate:"nonzero"`
+	Partner    entity.Pokemon `validate:"nonzero"`
+	CreatedAt  int64          `validate:"nonzero"`
 }
 
-func NewGame(playerName string, partner entity.Pokemon) (*Game, error) {
-	// TODO
-	return nil, nil
+func (c GameConfig) Validate() error {
+	return validator.Validate(c)
+}
+
+// AdvanceScenario is used for advancing current game scenario. It will
+// calculate the next scenario based on game current condition. Beside
+// updating game internal scenario into the new one, it also returns the
+// new scenario value.
+func (g *Game) AdvanceScenario() Scenario {
+	// determine next scenario
+	nextScenario := g.Scenario
+	if g.BattleWon >= 3 {
+		nextScenario = END_GAME
+	} else if g.BattleWon == 2 {
+		nextScenario = BATTLE_3
+	} else if g.BattleWon == 1 {
+		nextScenario = BATTLE_2
+	}
+	// update internal scenario & return its value
+	g.Scenario = nextScenario
+	return g.Scenario
+}
+
+// IncBattleWon is used for incrementing number of battle won
+func (g *Game) IncBattleWon() {
+	g.BattleWon++
+}
+
+func NewGame(cfg GameConfig) (*Game, error) {
+	err := cfg.Validate()
+	if err != nil {
+		return nil, err
+	}
+	g := &Game{
+		ID:         uuid.NewString(),
+		PlayerName: cfg.PlayerName,
+		Partner:    cfg.Partner,
+		CreatedAt:  cfg.CreatedAt,
+		BattleWon:  0,
+		Scenario:   BATTLE_1,
+	}
+	return g, nil
 }
