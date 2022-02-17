8edccab29ffa32bae3323ad86768a6c690b87a7b
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-02-17 21:04:05 +0300

    [build] finish implement new game test;

--- internal/domain/playing/game_test.go
+++ internal/domain/playing/game_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/riandyrn/pokebattle/internal/domain/entity"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestValidateGameConfig(t *testing.T) {
@@ -57,15 +58,53 @@ func TestValidateGameConfig(t *testing.T) {
 	for _, testCase := range testCases {
 		t.Run(testCase.Name, func(t *testing.T) {
 			err := testCase.Config.Validate()
-			if (err != nil) != testCase.IsError {
-				t.Fail()
-			}
+			assert.Equal(t, testCase.IsError, (err != nil), "unexpected error")
 		})
 	}
 }
 
 func TestNewGame(t *testing.T) {
-	// TODO
+	// define function for validating game
+	validateGame := func(t *testing.T, game Game, cfg GameConfig) {
+		assert.NotEmpty(t, game.ID, "game id is empty")
+		assert.Equal(t, cfg.PlayerName, game.PlayerName, "mismatch player name")
+		assert.Equal(t, cfg.Partner, game.Partner, "mismatch partner")
+		assert.Equal(t, cfg.CreatedAt, game.CreatedAt, "mismatch created_at")
+		assert.Equal(t, 0, game.BattleWon, "mismatch battle_won")
+		assert.Equal(t, BATTLE_1, game.Scenario, "mismatch scenario")
+	}
+	// define test cases
+	testCases := []struct {
+		Name    string
+		Config  GameConfig
+		IsError bool
+	}{
+		{
+			Name:    "Invalid Config",
+			Config:  GameConfig{},
+			IsError: true,
+		},
+		{
+			Name: "Valid Config",
+			Config: GameConfig{
+				PlayerName: "Riandy R.N",
+				Partner:    newSamplePokemon(),
+				CreatedAt:  time.Now().Unix(),
+			},
+			IsError: false,
+		},
+	}
+	// execute test cases
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			game, err := NewGame(testCase.Config)
+			assert.Equal(t, testCase.IsError, (err != nil), "unexpected error")
+			if game == nil {
+				return
+			}
+			validateGame(t, *game, testCase.Config)
+		})
+	}
 }
 
 func TestAdvanceScenario(t *testing.T) {
