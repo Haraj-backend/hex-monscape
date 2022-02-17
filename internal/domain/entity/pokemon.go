8cc9826cc3d95954b38f86ff4e59c6e8ac59b401
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-02-18 00:58:27 +0300

    [build] finish pokemon method;

--- internal/domain/entity/pokemon.go
+++ internal/domain/entity/pokemon.go
@@ -1,26 +1,37 @@
 package entity
 
+const minDamage = 5
+
 type Pokemon struct {
-	ID          string
-	Name        string
-	BattleStats BattleStats
-	AvatarURL   string
+	ID          string      `json:"id"`
+	Name        string      `json:"name"`
+	BattleStats BattleStats `json:"battle_stats"`
+	AvatarURL   string      `json:"avatar_url"`
 }
 
 func (p Pokemon) IsDead() bool {
-	// TODO
-	return false
+	return p.BattleStats.Health <= 0
 }
 
+// InflictDamage is used for inflicting damage to self based
+// on given enemy. Returned the damage amount.
 func (p *Pokemon) InflictDamage(enemy Pokemon) (int, error) {
-	// TODO
-	return 0, nil
+	dmg := max(enemy.BattleStats.Attack-p.BattleStats.Defense, minDamage)
+	p.BattleStats.Health -= dmg
+	return dmg, nil
 }
 
 type BattleStats struct {
-	Health    int
-	MaxHealth int
-	Attack    int
-	Defense   int
-	Speed     int
+	Health    int `json:"health"`
+	MaxHealth int `json:"max_health"`
+	Attack    int `json:"attack"`
+	Defense   int `json:"defense"`
+	Speed     int `json:"speed"`
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
