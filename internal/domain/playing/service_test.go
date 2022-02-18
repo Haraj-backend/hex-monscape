8496d3491709f5b4a5d695e0a273bb94ac76d8e7
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-02-18 16:48:18 +0300

    [build] finish implement test for playing storage;

--- internal/domain/playing/service_test.go
+++ internal/domain/playing/service_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/riandyrn/pokebattle/internal/domain/entity"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewService(t *testing.T) {
@@ -61,13 +62,85 @@ func TestNewService(t *testing.T) {
 	}
 }
 
-func TestServiceGetAvailablePartners(t *testing.T) {}
+func TestServiceGetAvailablePartners(t *testing.T) {
+	// initialize new service
+	svc, partners := newNewService()
+	// get available partners
+	retPartners, err := svc.GetAvailablePartners(context.Background())
+	require.NoError(t, err, "unexpected error")
+	// check returned partners
+	require.Equal(t, partners, retPartners, "mismatch partners")
+}
 
-func TestServiceNewGame(t *testing.T) {}
+func TestServiceNewGame(t *testing.T) {
+	// initialize new service
+	svc, partners := newNewService()
+	// create new game
+	partner := partners[0]
+	game, err := svc.NewGame(context.Background(), "Riandy R.N", partner.ID)
+	require.NoError(t, err, "unexpected error")
+	// validate returned game with stored game, this is to make sure the game
+	// is also stored on storage
+	storedGame, err := svc.gameStorage.GetGame(context.Background(), game.ID)
+	require.NoError(t, err, "unexpected error")
+	require.Equal(t, *game, *storedGame, "mismatch game")
+}
 
-func TestServiceGetGame(t *testing.T) {}
+func TestServiceGetGame(t *testing.T) {
+	// initialize new service
+	svc, partners := newNewService()
+	// create new game
+	partner := partners[0]
+	game, err := svc.NewGame(context.Background(), "Riandy R.N", partner.ID)
+	require.NoError(t, err, "unexpected error")
+	// define test cases
+	testCases := []struct {
+		Name   string
+		GameID string
+		ExpErr error
+	}{
+		{
+			Name:   "Test Game Not Found",
+			GameID: game.ID + "abc",
+			ExpErr: ErrGameNotFound,
+		},
+		{
+			Name:   "Test Game Found",
+			GameID: game.ID,
+			ExpErr: nil,
+		},
+	}
+	// execute test cases
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			retGame, err := svc.GetGame(context.Background(), testCase.GameID)
+			assert.Equal(t, testCase.ExpErr, err, "mismatch error")
+			if retGame == nil {
+				return
+			}
+			assert.Equal(t, game, retGame, "mismatch game")
+		})
+	}
+}
 
-func TestServiceAdvanceScenario(t *testing.T) {}
+func TestServiceAdvanceScenario(t *testing.T) {
+	// initialize new service
+	svc, partners := newNewService()
+	// create new game
+	partner := partners[0]
+	game, _ := svc.NewGame(context.Background(), "Riandy R.N", partner.ID)
+	// won battle 3 times, this should trigger condition to advance game scenario
+	for i := 0; i < 3; i++ {
+		game.IncBattleWon()
+	}
+	svc.gameStorage.SaveGame(context.Background(), *game)
+	// advance scenario
+	advGame, _ := svc.AdvanceScenario(context.Background(), game.ID)
+	require.NotEqual(t, game.Scenario, advGame.Scenario, "scenario still equal")
+	// game in storage should be advanced as well
+	storedGame, _ := svc.gameStorage.GetGame(context.Background(), game.ID)
+	require.Equal(t, *advGame, *storedGame, "mismatch game")
+}
 
 type mockGameStorage struct {
 	data map[string]Game
@@ -118,4 +191,30 @@ func newMockPartnerStorage(partners []entity.Pokemon) *mockPartnerStorage {
 		data[partner.ID] = partner
 	}
 	return &mockPartnerStorage{partnerMap: data}
+}
+
+func newNewService() (*Service, []entity.Pokemon) {
+	// generate partners
+	partners := []entity.Pokemon{
+		{
+			ID:   "b1c87c5c-2ac3-471d-9880-4812552ee15d",
+			Name: "Pikachu",
+			BattleStats: entity.BattleStats{
+				Health:    100,
+				MaxHealth: 100,
+				Attack:    25,
+				Defense:   5,
+				Speed:     10,
+			},
+			AvatarURL: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+		},
+	}
+	// initialize service
+	cfg := ServiceConfig{
+		GameStorage:    newMockGameStorage(),
+		PartnerStorage: newMockPartnerStorage(partners),
+	}
+	svc, _ := NewService(cfg)
+
+	return svc, partners
 }
