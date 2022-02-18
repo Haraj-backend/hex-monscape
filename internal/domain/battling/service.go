5417378ef9cf57299532a4abb884fa6ba4dc5162
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-02-18 20:48:06 +0300

    [build] finish implement start battle & get battle in battling service;

--- internal/domain/battling/service.go
+++ internal/domain/battling/service.go
@@ -3,7 +3,11 @@ package battling
 import (
 	"context"
 	"errors"
+	"fmt"
+	"math/rand"
+	"time"
 
+	"github.com/Haraj-backend/hex-pokebattle/internal/domain/playing"
 	"gopkg.in/validator.v2"
 )
 
@@ -20,16 +24,62 @@ type Service struct {
 }
 
 // StartBattle is used for starting new battle for given game id. Battle could only
-// be started when previous battle is not found or already ended.
+// be started when there is no previous battle or it is already ended.
 func (s *Service) StartBattle(ctx context.Context, gameID string) (*Battle, error) {
-	// TODO
-	return nil, nil
+	// get existing battle, make sure there is no ongoing battle
+	game, battle, err := s.getBattle(ctx, gameID)
+	if err != nil && !errors.Is(err, ErrBattleNotFound) {
+		return nil, err
+	}
+	if battle != nil {
+		return nil, ErrInvalidBattleState
+	}
+	// get possible enemies, choose it randomly
+	enemies, err := s.pokemonStorage.GetPossibleEnemies(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get possible enemies due: %w", err)
+	}
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	enemy := enemies[rnd.Intn(len(enemies))]
+	// create new battle instance
+	battle, err = NewBattle(BattleConfig{
+		GameID:  gameID,
+		Partner: game.Partner,
+		Enemy:   &enemy,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to create new battle instance due: %w", err)
+	}
+	// save newly created battle instance to storage
+	err = s.battleStorage.SaveBattle(ctx, *battle)
+	if err != nil {
+		return nil, fmt.Errorf("unable to save battle into storage due: %w", err)
+	}
+	return battle, nil
 }
 
-// GetBattle returns battle for given game id.
+// GetBattle returns ongoing battle for given game id.
 func (s *Service) GetBattle(ctx context.Context, gameID string) (*Battle, error) {
-	// TODO
-	return nil, nil
+	_, battle, err := s.getBattle(ctx, gameID)
+	return battle, err
+}
+
+func (s *Service) getBattle(ctx context.Context, gameID string) (*playing.Game, *Battle, error) {
+	game, err := s.gameStorage.GetGame(ctx, gameID)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to get game due: %w", err)
+	}
+	if game == nil {
+		return nil, nil, ErrGameNotFound
+	}
+	battle, err := s.battleStorage.GetBattle(ctx, gameID)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to get battle due: %w", err)
+	}
+	if battle == nil || battle.IsEnded() {
+		return nil, nil, ErrBattleNotFound
+	}
+	return game, battle, nil
 }
 
 // DecideTurn is used for deciding turn for the battle. There are 3 possible outcome
