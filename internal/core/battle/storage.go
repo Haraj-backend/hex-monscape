3e09b5dd42f4270935ba5ca28f491e21cf756750
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-03-06 16:52:41 +0300

    doc: add comments to storage interfaces in battle core, these comments will be used as a guide to implement the interfaces;

--- internal/core/battle/storage.go
+++ internal/core/battle/storage.go
@@ -7,15 +7,25 @@ import (
 )
 
 type BattleStorage interface {
+	// GetBattle returns battle instance for given gameID from storage. Returns nil
+	// when battle instance is not found.
 	GetBattle(ctx context.Context, gameID string) (*Battle, error)
+
+	// SaveBattle is used for saving given battle instance into storage. If battle
+	// instance is already exists in the storage, it will be overwritten.
 	SaveBattle(ctx context.Context, b Battle) error
 }
 
 type GameStorage interface {
+	// GetGame returns game instance for given gameID from storage. Returns nil
+	// when given gameID is not found in database.
 	GetGame(ctx context.Context, gameID string) (*entity.Game, error)
+
+	// Save is used for saving game instance in storage.
 	SaveGame(ctx context.Context, game entity.Game) error
 }
 
 type PokemonStorage interface {
+	// GetPossibleEnemies returns all possible enemies in the game.
 	GetPossibleEnemies(ctx context.Context) ([]entity.Pokemon, error)
 }
