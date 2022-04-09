f677c5e412859815a37ab56d512422f9e230ff0f
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-04-09 22:17:26 +0300

    feat: finish simplify GetBattle() & SaveBattle(); point mysql volumes to correct dir;

--- internal/driven/storage/mysql/battlestrg/model.go
+++ internal/driven/storage/mysql/battlestrg/model.go
@@ -2,27 +2,88 @@ package battlestrg
 
 import (
 	"github.com/Haraj-backend/hex-pokebattle/internal/core/battle"
-	"github.com/Haraj-backend/hex-pokebattle/internal/driven/storage/mysql/shared"
+	"github.com/Haraj-backend/hex-pokebattle/internal/core/entity"
 )
 
 type battleRow struct {
-	GameID         string          `db:"game_id"`
-	PartnerLastDmg int             `db:"partner_last_damage"`
-	EnemyLastDmg   int             `db:"enemy_last_damage"`
-	State          battle.State    `db:"state"`
-	Partner        *shared.PokeRow `db:"partner"`
-	Enemy          *shared.PokeRow `db:"enemy"`
+	GameID            string `db:"game_id"`
+	State             string `db:"state"`
+	PartnerPokemonID  string `db:"partner_pokemon_id"`
+	PartnerName       string `db:"partner_name"`
+	PartnerMaxHealth  int    `db:"partner_max_health"`
+	PartnerHealth     int    `db:"partner_health"`
+	PartnerAttack     int    `db:"partner_attack"`
+	PartnerDefense    int    `db:"partner_defense"`
+	PartnerSpeed      int    `db:"partner_speed"`
+	PartnerAvatarURL  string `db:"partner_avatar_url"`
+	PartnerLastDamage int    `db:"partner_last_damage"`
+	EnemyPokemonID    string `db:"enemy_pokemon_id"`
+	EnemyName         string `db:"enemy_name"`
+	EnemyMaxHealth    int    `db:"enemy_max_health"`
+	EnemyHealth       int    `db:"enemy_health"`
+	EnemyAttack       int    `db:"enemy_attack"`
+	EnemyDefense      int    `db:"enemy_defense"`
+	EnemySpeed        int    `db:"enemy_speed"`
+	EnemyAvatarURL    string `db:"enemy_avatar_url"`
+	EnemyLastDamage   int    `db:"enemy_last_damage"`
 }
 
-func (r *battleRow) ToBattle() *battle.Battle {
+func (r battleRow) ToBattle() *battle.Battle {
 	return &battle.Battle{
 		GameID: r.GameID,
-		LastDamage: battle.LastDamage{
-			Partner: r.PartnerLastDmg,
-			Enemy:   r.EnemyLastDmg,
+		State:  battle.State(r.State),
+		Partner: &entity.Pokemon{
+			ID:   r.PartnerPokemonID,
+			Name: r.PartnerName,
+			BattleStats: entity.BattleStats{
+				Health:    r.PartnerHealth,
+				MaxHealth: r.PartnerMaxHealth,
+				Attack:    r.PartnerAttack,
+				Defense:   r.PartnerDefense,
+				Speed:     r.PartnerSpeed,
+			},
+			AvatarURL: r.PartnerAvatarURL,
 		},
-		State:   r.State,
-		Partner: r.Partner.ToPokemon(),
-		Enemy:   r.Enemy.ToPokemon(),
+		Enemy: &entity.Pokemon{
+			ID:   r.EnemyPokemonID,
+			Name: r.EnemyName,
+			BattleStats: entity.BattleStats{
+				Health:    r.EnemyHealth,
+				MaxHealth: r.EnemyMaxHealth,
+				Attack:    r.EnemyAttack,
+				Defense:   r.EnemyDefense,
+				Speed:     r.EnemySpeed,
+			},
+			AvatarURL: r.EnemyAvatarURL,
+		},
+		LastDamage: battle.LastDamage{
+			Partner: r.PartnerLastDamage,
+			Enemy:   r.EnemyLastDamage,
+		},
+	}
+}
+
+func newBattleRow(b battle.Battle) battleRow {
+	return battleRow{
+		GameID:            b.GameID,
+		State:             string(b.State),
+		PartnerPokemonID:  b.Partner.ID,
+		PartnerName:       b.Partner.Name,
+		PartnerMaxHealth:  b.Partner.BattleStats.MaxHealth,
+		PartnerHealth:     b.Partner.BattleStats.Health,
+		PartnerAttack:     b.Partner.BattleStats.Attack,
+		PartnerDefense:    b.Partner.BattleStats.Defense,
+		PartnerSpeed:      b.Partner.BattleStats.Speed,
+		PartnerAvatarURL:  b.Partner.AvatarURL,
+		PartnerLastDamage: b.LastDamage.Partner,
+		EnemyPokemonID:    b.Enemy.ID,
+		EnemyName:         b.Enemy.Name,
+		EnemyMaxHealth:    b.Enemy.BattleStats.MaxHealth,
+		EnemyHealth:       b.Enemy.BattleStats.Health,
+		EnemyAttack:       b.Enemy.BattleStats.Attack,
+		EnemyDefense:      b.Enemy.BattleStats.Defense,
+		EnemySpeed:        b.Enemy.BattleStats.Speed,
+		EnemyAvatarURL:    b.Enemy.AvatarURL,
+		EnemyLastDamage:   b.LastDamage.Enemy,
 	}
 }
