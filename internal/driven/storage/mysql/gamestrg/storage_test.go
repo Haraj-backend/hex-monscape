909132ae47267d7593e5be17879ac6c1b399c9ca
Ilham Syahid S <ilhamsyahids@gmail.com>
2022-04-11 14:36:36 +0700

    test(mysql-adapter): add game storage test

--- internal/driven/storage/mysql/gamestrg/storage_test.go
+++ internal/driven/storage/mysql/gamestrg/storage_test.go
@@ -2,60 +2,91 @@ package gamestrg
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"testing"
 
-	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
+
 	"github.com/Haraj-backend/hex-pokebattle/internal/core/entity"
+	"github.com/Haraj-backend/hex-pokebattle/internal/driven/storage/mysql/shared"
 	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/require"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func TestShouldGetGame(t *testing.T) {
-	db0, mock, err := sqlmock.New()
-	db := sqlx.NewDb(db0, "mysql")
+const envKeySQLDSN = "SQL_DSN"
 
+func newSQLClient() (*sqlx.DB, error) {
+	sqlDSN := os.Getenv(envKeySQLDSN)
+	sqlClient, err := sqlx.Connect("mysql", sqlDSN)
 	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		return nil, fmt.Errorf("unable to initialize sql client due: %w", err)
 	}
-	defer db.Close()
-
-	gamestrg := New(db)
-
-	columns := []string{"id", "player_name", "created_at", "battle_won", "scenario",
-		"partner.id", "partner.name", "partner.avatar_url", "partner.max_health", "partner.attack", "partner.defense", "partner.speed"}
-	gameId := "b1c87c5c-2ac3-471d-9880-4812552ee15d"
-
-	mock.ExpectQuery("^SELECT (.+) FROM games g").
-		WithArgs(gameId).
-		WillReturnRows(
-			sqlmock.NewRows(columns).
-				FromCSVString("1,player1,1646205996,1,BATTLE_3,b1c87c5c-2ac3-471d-9880-4812552ee15d,Pikachu,https://example.com/025.png,100,49,49,45"))
-
-	ctx := context.Background()
-
-	if _, err = gamestrg.GetGame(ctx, gameId); err != nil {
-		t.Errorf("error was not expected while getting partners: %s", err)
-	}
-
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
-	}
+	return sqlClient, nil
 }
 
-func TestSaveGameInsert(t *testing.T) {
-	db0, mock, err := sqlmock.New()
-	db := sqlx.NewDb(db0, "mysql")
+func TestSaveGame(t *testing.T) {
+	// initialize sql client
+	sqlClient, err := newSQLClient()
+	require.NoError(t, err)
+	// initialize storage
+	strg, err := New(shared.Config{SQLClient: sqlClient})
+	require.NoError(t, err)
+	// save Game
+	g := newGame()
+	err = strg.SaveGame(context.Background(), g)
+	require.NoError(t, err)
+	// check whether Game exists on database
+	savedGame, err := strg.GetGame(context.Background(), g.ID)
+	fmt.Println(savedGame)
+	require.NoError(t, err)
+	// check whether Game data is match
+	require.Equal(t, g, *savedGame)
+}
 
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
+func TestGetGame(t *testing.T) {
+	// initialize sql client
+	sqlClient, err := newSQLClient()
+	require.NoError(t, err)
+	// initialize storage
+	strg, err := New(shared.Config{SQLClient: sqlClient})
+	require.NoError(t, err)
+	// save Game
+	g := newGame()
+	err = strg.SaveGame(context.Background(), g)
+	require.NoError(t, err)
+	// override Game battle won
+	g.BattleWon = 1
+	// save again
+	err = strg.SaveGame(context.Background(), g)
+	require.NoError(t, err)
+	// check whether Game exists on database
+	savedGame, err := strg.GetGame(context.Background(), g.ID)
+	require.NoError(t, err)
+	// check whether Game data is match
+	require.Equal(t, g, *savedGame)
+}
 
-	gamestrg := New(db)
+func TestGetGameNotFound(t *testing.T) {
+	// initialize sql client
+	sqlClient, err := newSQLClient()
+	require.NoError(t, err)
+	// initialize storage
+	strg, err := New(shared.Config{SQLClient: sqlClient})
+	require.NoError(t, err)
+	// check whether game exists on database
+	savedGame, err := strg.GetGame(context.Background(), uuid.NewString())
+	require.NoError(t, err)
+	require.Nil(t, savedGame)
+}
 
+func newGame() entity.Game {
 	partner := entity.Pokemon{
 		ID:        "b1c87c5c-2ac3-471d-9880-4812552ee15d",
 		Name:      "Pikachu",
-		AvatarURL: "https://example.com/025.png",
+		AvatarURL: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
 		BattleStats: entity.BattleStats{
 			Health:    100,
 			MaxHealth: 100,
@@ -65,89 +96,12 @@ func TestSaveGameInsert(t *testing.T) {
 		},
 	}
 
-	game := entity.Game{
-		ID:         "b1c87c5c-2ac3-471d-9880-4812552ee15d",
+	return entity.Game{
+		ID:         uuid.NewString(),
 		PlayerName: "player1",
 		CreatedAt:  1646205996,
-		BattleWon:  1,
+		BattleWon:  0,
 		Scenario:   "BATTLE_3",
 		Partner:    &partner,
-	}
-
-	mock.ExpectQuery("^SELECT (.+) FROM games").
-		WithArgs(game.ID).
-		WillReturnRows(sqlmock.NewRows([]string{"id"}))
-
-	mock.ExpectBegin()
-	mock.ExpectExec("^INSERT INTO games").
-		WithArgs(game.ID, game.PlayerName, game.CreatedAt, game.BattleWon, game.Scenario, game.Partner.ID).
-		WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectCommit()
-
-	ctx := context.Background()
-
-	if err = gamestrg.SaveGame(ctx, game); err != nil {
-		t.Errorf("error was not expected while getting partners: %s", err)
-	}
-
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
-	}
-}
-
-func TestSaveGameUpdate(t *testing.T) {
-	db0, mock, err := sqlmock.New()
-	db := sqlx.NewDb(db0, "mysql")
-
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-
-	gamestrg := New(db)
-
-	partner := entity.Pokemon{
-		ID:        "b1c87c5c-2ac3-471d-9880-4812552ee15d",
-		Name:      "Pikachu",
-		AvatarURL: "https://example.com/025.png",
-		BattleStats: entity.BattleStats{
-			Health:    100,
-			MaxHealth: 100,
-			Attack:    49,
-			Defense:   49,
-			Speed:     45,
-		},
-	}
-
-	game := entity.Game{
-		ID:         "b1c87c5c-2ac3-471d-9880-4812552ee15d",
-		PlayerName: "player1",
-		CreatedAt:  1646205996,
-		BattleWon:  1,
-		Scenario:   "BATTLE_3",
-		Partner:    &partner,
-	}
-
-	columns := []string{"id"}
-
-	rows := mock.NewRows(columns).
-		AddRow(game.ID)
-
-	ctx := context.Background()
-
-	mock.ExpectQuery("^SELECT (.+) FROM games").
-		WithArgs(game.ID).
-		WillReturnRows(rows)
-
-	mock.ExpectExec("^UPDATE games").
-		WithArgs(game.PlayerName, game.CreatedAt, game.BattleWon, game.Scenario, game.Partner.ID, game.ID).
-		WillReturnResult(sqlmock.NewResult(1, 1))
-
-	if err = gamestrg.SaveGame(ctx, game); err != nil {
-		t.Errorf("error was not expected while getting partners: %s", err)
-	}
-
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
