454624409999f407381146c9634964f21c365976
Riandy Rahman Nugraha <riandyrn@gmail.com>
2022-05-15 11:44:13 +0300

    tidy: clean up deployment dir; break & rename scripts for better clarity;

---
+++ deploy/local/deployment/ddb-write-data.sh
@@ -0,0 +1,2 @@
+awslocal dynamodb batch-write-item --request-item file:///var/partner-batch-write-items.json
+awslocal dynamodb batch-write-item --request-item file:///var/enemy-batch-write-items.json
