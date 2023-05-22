6296ca4928333311bf228672cb1ac224db0d5465
Muhammad Izzuddin al Fikri <muhammadizzuddinalfikri@gmail.com>
2023-05-22 13:05:54 +0000

    feat: add `pinia` as state mgmt

--- cmd/web-nuxt/nuxt.config.ts
+++ cmd/web-nuxt/nuxt.config.ts
@@ -2,6 +2,20 @@
 export default defineNuxtConfig({
   ssr: false,
   css: ["@/assets/css/app.css"],
+  modules: [
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: [
+          // automatically imports `defineStore`
+          "defineStore", // import { defineStore } from 'pinia'
+          // automatically imports `defineStore` as `definePiniaStore`
+          ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
+        ],
+      },
+    ],
+    "@pinia-plugin-persistedstate/nuxt",
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
